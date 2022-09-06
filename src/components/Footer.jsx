20811f3f4aaf0a42d25e29d4e9f19c2d72964f2f
VictorSilva27 <victoradaosilva2@gmail.com>
2022-09-05 22:14:50 -0300

    Adicionando footer e modificando css

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,8 +1,36 @@
-import React from 'react'
+import React from 'react';
+import { GoMarkGithub } from 'react-icons/go';
+import { FiLinkedin } from 'react-icons/fi';
+import '../css/Footer.css';
 
 function Footer() {
   return (
-    <div>Footer</div>
+    <div className="footer-page">
+      <h3>Social Networks</h3>
+      <div className="social-networks">
+        <a
+          href="https://github.com/VictorSilva27"
+          target="_blank"
+          rel="noreferrer"
+          className="icon-github" 
+        >
+          <GoMarkGithub className="icon-github" />
+          GitHub
+        </a>
+        <a
+          href="https://www.linkedin.com/in/victor-silva027/"
+          target="_blank"
+          rel="noreferrer"
+          className="icon-linkedin"
+        >
+          <FiLinkedin className="icon-linkedin"/>
+          LinkedIn
+        </a>
+      </div>
+
+      <p className="p-footer">
+        Desenvolvido por <span className="my-name">Victor Silva</span>, 2022 © - Projeto Star Wars.</p>
+    </div>
   )
 }
 
